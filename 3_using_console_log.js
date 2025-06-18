e6191a7e712e77af77e2b5717706e72105058761
Rishon <rishonrajesh13@gmail.com>
2025-06-18 16:08:26 -0700

    Notes on console.log() and use of comments is added

---
+++ 3_using_console_log.js
@@ -0,0 +1,19 @@
+// Before discussing the console.log() function, let us first describe a JS feature that we have been using for a while now
+// This feature is the comments
+// Single-line comments in JS are created with two forward slashes
+/* Multi
+Line comments
+are created with a forward slash and asterisk as seen here
+and closed with an asterisk followed by a forward slash as seen
+here */
+
+// Now , what does console.log() do?
+// console.log() is a built-in function in JavaScript used to print output to the browser’s console
+// It is used mainly for debugging and diagnostics.
+// It doesn't affect the web page itself — it prints messages to the developer console
+// Example below:
+console.log('Hello World');
+
+// You can log one or more values.
+// It can log strings, numbers, objects, arrays, functions, or results of expressions.
+// Syntax: console.log(value1, value2, ..., valueN);
