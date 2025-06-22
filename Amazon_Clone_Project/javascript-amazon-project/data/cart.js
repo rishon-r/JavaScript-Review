7cb2a29ee038f8d7404b84856addb8b716aff7a2
Rishon <rishonrajesh13@gmail.com>
2025-06-22 14:47:11 -0700

    Amazon Project: Cart feature created, some modularization added

--- Amazon_Clone_Project/javascript-amazon-project/data/cart.js
+++ Amazon_Clone_Project/javascript-amazon-project/data/cart.js
@@ -1,4 +1,11 @@
-export let cart=[];
+export let cart= JSON.parse(localStorage.getItem('cart')) || [{
+  productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+  quantity: 2
+},
+{
+  productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+  quantity: 1
+}];
 
 export function addToCart(productId){
 
@@ -20,4 +27,23 @@ export function addToCart(productId){
     console.log(cart);
   }
 
+  saveToStorage();
+
+}
+
+export function removeFromCart(productId){
+  const newCart= [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId){
+      newCart.push(cartItem);
+    }
+  })
+
+  cart=newCart;
+  saveToStorage();
+}
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
 }
