663506ed0b036dd49a051f5aa631c7e4ede12e0e
Rishon <rishonrajesh13@gmail.com>
2025-06-23 13:29:49 -0700

    Delivery options made interactive

--- Amazon_Clone_Project/javascript-amazon-project/data/cart.js
+++ Amazon_Clone_Project/javascript-amazon-project/data/cart.js
@@ -50,3 +50,19 @@ export function removeFromCart(productId){
 function saveToStorage() {
   localStorage.setItem('cart', JSON.stringify(cart));
 }
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId){
+      matchingItem=cartItem;
+    }
+  });
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
+
+  saveToStorage();
+}
+
+
--- Amazon_Clone_Project/javascript-amazon-project/scripts/checkout.js
+++ Amazon_Clone_Project/javascript-amazon-project/scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart, removeFromCart} from '../data/cart.js'
+import {cart, removeFromCart, updateDeliveryOption} from '../data/cart.js'
 import {products} from  '../data/products.js'
 import {formatCurrency} from  './utils/money.js'
 import dayJS from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js'
@@ -120,7 +120,8 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
       const isChecked= deliveryOption.id==cartItem.deliveryOptionId;
 
       html+=`
-      <div class="delivery-option">
+      <div class="delivery-option js-delivery-option" data-product-id="${matchingProduct.id}" 
+                  data-delivery-option-id="${deliveryOption.id}">
                   <input type="radio" ${isChecked ? 'checked' : ''}
                     class="delivery-option-input"
                     name="delivery-option-${matchingProduct.id }">
@@ -135,8 +136,15 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
                 </div>
       `;
     })
-  console.log(html);
+  
   return html;
 }
 
-
+document.querySelectorAll('.js-delivery-option')
+  .forEach((element) => {
+    element.addEventListener('click', () => {
+      const {productId, deliveryOptionId} = element.dataset;
+      updateDeliveryOption(productId, deliveryOptionId);
+    })
+  }
+)
