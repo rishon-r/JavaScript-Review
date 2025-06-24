f28a6d6b6be6f0618ddbc15a98df99ae61649cca
Rishon <rishonrajesh13@gmail.com>
2025-06-24 13:19:40 -0700

    Notes on OOP added

---
+++ 14_OOP.js
@@ -0,0 +1,13 @@
+// OOP- Object Oriented Programming
+// A different method of programming that involves organizing your code into objects
+// So far, we have used PROCEDURAL PROGRAMMING
+// A procedure refers to a set of step-by-step instructions
+// in PROCEDURAL PROGRAMMING we organize our code into functions
+
+// In OOP, we group our data and functions into objects
+// Why do we use OOP?
+// OOP tries to represent the real world
+// E.g in real life, a cart can have properties inside it and we can perform actions to modify a cart
+// Some developers feel that this makes the code more intuitive
+
+// Another important reason to use objects is that it is easy to create multiple objects
--- Amazon_Clone_Project/javascript-amazon-project/scripts/checkout.js
+++ Amazon_Clone_Project/javascript-amazon-project/scripts/checkout.js
@@ -1,5 +1,7 @@
-import {renderOrderSummary} from './checkout/orderSummary.js'
-import {renderPaymentSummary} from './checkout/paymentSummary.js'
+import {renderOrderSummary} from './checkout/orderSummary.js';
+import {renderPaymentSummary} from './checkout/paymentSummary.js';
+import '../data/cart-OOP.js'; // THis syntax just runs all the code in a file
+
 
 renderOrderSummary();
 renderPaymentSummary();
